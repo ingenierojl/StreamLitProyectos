ccb204b0a5a8d2dd93b4bef1619838071e6e6ae6
ingenierojl <34725822+ingenierojl@users.noreply.github.com>
2023-10-02 06:01:35 +0000

    traducir

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ print(streamlit_option_menu.__file__)
 from streamlit_option_menu import option_menu
 
 
-selected = option_menu(None, ["Home", "Experience", "Education", "Projects", "Skills--", "Language"],
+selected = option_menu(None, ["Home", "Experience", "Education", "Projects", "Skills", "Language"],
     icons=['house', 'briefcase', 'mortarboard', 'clipboard-data', 'gear', 'translate'], 
     default_index=0, orientation="horizontal") 
 
